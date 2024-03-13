6ae03bd031507f2fa3add4f756a074784c1e1357
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-03-13 13:30:33 +0000

    Ajouter autre champs pour le resultat

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-const display = document.querySelector(".display");
-const buttons = document.querySelectorAll(".buttons");
-const specialChars = ["%","*","/","-","+","="];
-let output = "";
+document.addEventListener("DOMContentLoaded", () => {
+    const display = document.querySelector(".display");
+    const operationDisplay = document.querySelector(".operation-display");
+    const resultDisplay = document.querySelector(".result-display");
+    const buttons = document.querySelectorAll(".button");
+    const specialChars = ["%", "*", "/", "-", "+", "="];
+    let operationText = "";
+    let output = "";
 
-//define function to calculate based on button click
-const calculate = (btnValue) => {
-    if(btnValue === "=" && output !== "") {
+    const calculate = (btnValue) => {
+        if (btnValue === "=" && output !== "") {
+            output = eval(output.replace("%", "/100"));
+            operationText = display.value + " = " + output;
+            operationDisplay.textContent = operationText;
+            resultDisplay.textContent = output;
+        } else if (btnValue === "AC") {
+            output = "";
+            operationText = "";
+            operationDisplay.textContent = "";
+            resultDisplay.textContent = "";
+        } else if (btnValue === "DEL") {
+            output = output.toString().slice(0, -1);
+        } else {
+            if (output === "" && specialChars.includes(btnValue)) return;
+            output += btnValue;
+        }
 
-        output = eval(output.replace("%", "/100"));
-    }else if (btnValue === "AC") {
-        output = "";
-    } else if (btnValue === "DEL") {
-        output = output.toString().slice(0, -1);
-    } else {
-        if(output === "" && specialChars.includes(btnValue)) return;
-        output += btnValue;
-    }
+        display.value = output;
+    };
 
-    display.value = output;
-
-};
-
-//Add event
-buttons.forEach((button) => {
-button.addEventListener("click", (e)=> calculate(e.target.dataset.value));
-
-
+    buttons.forEach((button) => {
+        button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+    });
 });
